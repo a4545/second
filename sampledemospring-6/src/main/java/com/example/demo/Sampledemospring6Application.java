9a4d748a095f46e03d9ea2ea234696273cce7ccd
a4545 <araj85501@gmail.com>
2019-09-19 17:30:05 +0530

    done

---
+++ sampledemospring-6/src/main/java/com/example/demo/Sampledemospring6Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sampledemospring6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sampledemospring6Application.class, args);
+	}
+
+}
